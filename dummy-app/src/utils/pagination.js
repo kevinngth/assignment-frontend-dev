9b828360fbcbb923ef1e2a2a428a9ca6b4f2ccc8
kevinngth <kevin.ng.th@gmail.com>
2020-01-14 07:24:53 +0100

    task 3 solved

--- dummy-app/src/utils/pagination.js
+++ dummy-app/src/utils/pagination.js
@@ -1,5 +1,19 @@
 export function paginator(currentPage, totalPages) {
   let pages = [];
   // your code here, have fun!
+  let page = currentPage - 2;
+  if (currentPage <= 3) {
+    page = 1
+  } else if (totalPages - currentPage < 3) {
+    page = totalPages - 4
+  }
+  for ( let i = 0 ; i < 5 ; i++ ) {
+    pages.push(page)
+    if ( page == totalPages ) {
+      break
+    } else {
+      page++
+    }
+  }
   return pages;
 }
